db820bf30d33c7cd5f47d3af51d01af07b2053a5
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-07-02 19:51:02 -0400

    Added checks for validation.

--- cypress/e2e/default.cy.js
+++ cypress/e2e/default.cy.js
@@ -2,23 +2,56 @@ describe('ultimate form', () => {
   it('submits successfully', () => {
     cy.visit('/')
 
+    // STEP 1
     cy.get('h2').should('contain', 'Step 1')
-    cy.get('#firstName').type('Maria')
-    cy.get('#lastName').type('Sanchez')
+
+    cy.get('#firstName-helper-text').should('not.exist')
+    cy.get('#firstName').focus().blur()
+    cy.get('#firstName-helper-text').should('exist')
+
+    cy.get('#lastName-helper-text').should('not.exist')
+    cy.get('#lastName').focus().blur()
+    cy.get('#lastName-helper-text').should('exist')
+
+    // Submit shouldn't work.
+    cy.get('button').click()
+    cy.get('h2').should('contain', 'Step 1')
+
+    cy.get('#firstName').type('Maria').blur()
+    cy.get('#firstName-helper-text').should('not.exist')
+
+    // Submit shouldn't work.
+    cy.get('button').click()
+    cy.get('h2').should('contain', 'Step 1')
+
+    cy.get('#lastName').type('Sanchez').blur()
+    cy.get('#lastName-helper-text').should('not.exist')
     cy.get('button').click()
 
+    // STEP 2
     cy.url().should('contain', '/step2')
     cy.get('h2').should('contain', 'Step 2')
-    cy.get('#email').type('maria@example.com')
+
+    cy.get('#email-helper-text').should('not.exist')
+    cy.get('#email').focus().blur()
+    cy.get('#email-helper-text').should('exist')
+
+    cy.get('#email').type('maria@example.com').blur()
+    cy.get('#email-helper-text').should('not.exist')
+
+    cy.get('#phoneNumber').should('not.exist')
     cy.get('input[name=hasPhone]').check()
+    cy.get('#phoneNumber').should('exist')
     cy.get('#phoneNumber').type('555-555-5555')
     cy.get('button').click()
 
+    // STEP 3
     cy.url().should('contain', '/step3')
     cy.get('h2').should('contain', 'Step 3')
     // Skip upload for now.
     cy.get('button').click()
 
+    // RESULTS
     cy.url().should('contain', '/result')
     cy.get('h2').should('contain', 'Form Values')
     cy.get('table').within(() => {
